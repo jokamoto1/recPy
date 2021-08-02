cad13159f22076c70c38dce08809718f46928101
Jeremy Okamoto <okamotojeremy@gmail.com>
2021-08-02 10:22:36 -0700

    a

---
+++ file.py
@@ -0,0 +1,9 @@
+# this is a single line comment
+"""
+This is
+a multiline
+comment
+"""
+my_new_favorite_language = 'Python' # variable declaration, initialize string
+
+print(my_new_favorite_language)
